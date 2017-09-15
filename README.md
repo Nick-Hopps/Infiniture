f0a0d57f5df9a8a569f58fa15d28129984e26584
NickHopps <476335433@qq.com>
2017-09-15 23:39:21 +0800

    some adjust

--- README.md
+++
@@ -1,3 +0,0 @@
-# Wordpress主题
-
-Infiniture 是一个轻量，多栏，自适应的简约主题。
--- header.php
+++ header.php
@@ -16,7 +16,6 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
-	<link href='http://fonts.font.im/css?family=Quicksand' rel='stylesheet' type='text/css'>
 
 	<?php wp_head(); ?>
 </head>
